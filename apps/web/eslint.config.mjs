dc881eb25bbb89112a1b8b718be071d9237e6332
3shaan <3shaan.dev@proton.me>
2025-10-22 16:20:06 +0600

    feat(eslint): eslint configure

--- apps/web/eslint.config.mjs
+++ apps/web/eslint.config.mjs
@@ -1,25 +1,52 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import antfu from '@antfu/eslint-config';
+import nextPlugin from '@next/eslint-plugin-next';
+import jsxA11y from 'eslint-plugin-jsx-a11y';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default antfu(
   {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
+    react: true,
+    nextjs: true,
+    typescript: true,
+    lessOpinionated: true,
+    isInEditor: false,
+    stylistic: {
+      semi: true,
+    },
+    formatters: {
+      css: true,
+    },
+    ignores: ['migrations/**/*', 'next-env.d.ts'],
   },
-];
+  jsxA11y.flatConfigs.recommended,
+  {
+    plugins: {
+      '@next/next': nextPlugin,
+    },
+    rules: {
+      ...nextPlugin.configs.recommended.rules,
+      ...nextPlugin.configs['core-web-vitals'].rules,
+    },
+  },
+  {
+    rules: {
+      'antfu/no-top-level-await': 'off',
+      'style/brace-style': ['error', '1tbs'],
+      'ts/consistent-type-definitions': ['error', 'type'],
+      'react/prefer-destructuring-assignment': 'off',
+      'node/prefer-global/process': 'off',
+      'test/padding-around-all': 'error',
+      'test/prefer-lowercase-title': 'off',
+      'ts/no-redeclare': 'off',
+      'no-console': ['warn'],
+      'node/no-process-env': ['error'],
+      'perfectionist/sort-imports': ['error', {
+        tsconfigRootDir: '.',
+      }],
+      'unicorn/filename-case': ['error', {
+        case: 'kebabCase',
+        ignore: ['README.md'],
+      }],
+    },
+  },
 
-export default eslintConfig;
+);
